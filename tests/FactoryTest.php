bba3fa6ba0b1727a97abb398d45abbfde842f79e
siad007 <siad.ardroumli@gmail.com>
2014-08-03 16:25:49 +0200

    added test-case

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -9,10 +9,30 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
+    public function instantiateCloneCommand()
+    {
+        $cloneCommand = Factory::getInstance('clone');
+
+        $this->assertInstanceOf('Siad007\VersionControl\HG\Command\CloneCommand', $cloneCommand);
+    }
+
+    /**
+     * @test
+     */
     public function instantiateInitCommand()
     {
         $initCommand = Factory::getInstance('init');
 
-        $this->assertInstanceOf('\\Siad007\\VersionControl\\HG\\InitCommand', $initCommand);
+        $this->assertInstanceOf('Siad007\VersionControl\HG\Command\InitCommand', $initCommand);
+    }
+
+    /**
+     * @test
+     */
+    public function instantiatePathsCommand()
+    {
+        $pathsCommand = Factory::getInstance('paths');
+
+        $this->assertInstanceOf('Siad007\VersionControl\HG\Command\PathsCommand', $pathsCommand);
     }
 }
