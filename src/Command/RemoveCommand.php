72fb44e9a4d6ec1b647c3e7a64aefe386a1f4c67
siad007 <siad.ardroumli@googlemail.com>
2014-09-14 01:47:10 +0200

    Added remove command

---
+++ src/Command/RemoveCommand.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * VersionControl_HG
+ * Simple OO implementation for Mercurial.
+ *
+ * PHP Version 5.4
+ *
+ * @copyright 2014 Siad Ardroumli
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @link http://siad007.github.io/versioncontrol_hg
+ */
+
+namespace Siad007\VersionControl\HG\Command;
+
+/**
+ * Simple OO implementation for Mercurial.
+ *
+ * @author Siad Ardroumli <siad.ardroumli@gmail.com>
+ *
+ * @method boolean getAfter()
+ * @method void setAfter(boolean $flag)
+ * @method boolean getForce()
+ * @method void setForce(boolean $flag)
+ * @method array getInclude()
+ * @method void addInclude(string $pattern)
+ * @method array getExclude()
+ * @method void addExclude(string $pattern)
+ */
+class RemoveCommand extends AbstractCommand
+{
+    /**
+     * Available arguments for this command.
+     *
+     * @var array $arguments
+     */
+    protected $arguments = array(
+        'file' => array()
+    );
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var mixed $options
+     */
+    protected $options = array(
+        '--after'   => false,
+        '--force'   => false,
+        '--include' => array(),
+        '--exclude' => array()
+    );
+
+    /**
+     * @return array
+     */
+    public function getFile()
+    {
+        return $this->arguments['file'];
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return void
+     */
+    public function addFile($file)
+    {
+        $this->arguments['file'][] = escapeshellarg($file);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return sprintf(
+            "%s%s %s",
+            $this->name,
+            $this->assembleOptionString(),
+            implode(' ', $this->arguments['file'])
+        );
+    }
+}
---
+++ tests/Command/RemoveCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * VersionControl_HG
+ * Simple OO implementation for Mercurial.
+ *
+ * PHP Version 5.4
+ *
+ * @copyright 2014 Siad Ardroumli
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @link http://siad007.github.io/versioncontrol_hg
+ */
+
+namespace Siad007\VersionControl\HG\Tests\Command;
+
+use Siad007\VersionControl\HG\Factory;
+
+class RemoveCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function removeCommand()
+    {
+        $addremoveCmd = Factory::createRemove();
+        $addremoveCmd->addFile('C:\\xampp\\file1\\');
+        $addremoveCmd->addFile('C:\\xampp\\file2\\');
+        $addremoveCmd->setAfter(true);
+        $addremoveCmd->addInclude('includePattern');
+        $addremoveCmd->addExclude('excludePattern');
+        $addremoveCmd->setForce(true);
+
+        $file = '\'C:\xampp\file1\\\' \'C:\xampp\file2\\\'';
+        $expected = 'hg remove --after --force --include includePattern --exclude excludePattern ';
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $file = str_replace("'", '"', $file);
+        }
+
+        $this->assertSame($file, implode(' ', $addremoveCmd->getFile()));
+        $this->assertSame($expected . $file, $addremoveCmd->asString());
+    }
+}
